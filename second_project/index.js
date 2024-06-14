ccae831818ec518b1d3a67206900df2b4f66bb4d
Nirmal Pothuraj <nirmalpothuraj@pop-os.home>
2024-06-14 15:33:14 -0400

    Sample programs for the node js in linux

---
+++ second_project/index.js
@@ -0,0 +1,5 @@
+import color from 'chalk'
+import greetings from './greetings'
+//const greetings = require('./greetings.js')
+console.log(color.red("Hey There!.. starting my node journey againnpcl"))
+greetings.hello();
